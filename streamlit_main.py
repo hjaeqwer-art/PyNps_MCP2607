82aaa6033f647990c932c4f213f6eea85dcf28a2
hjaeqwer-art <hjaeqwer@gmail.com>
2026-08-25 20:25:56 +0900

    Update streamlit_main.py

--- streamlit_main.py
+++ streamlit_main.py
@@ -11,15 +11,13 @@ import re
 import streamlit as st
 
 # 한글폰트 설정
-try :
+try:
     if platform.system() == 'Windows':
-    # 윈도우인 경우
         font_name = font_manager.FontProperties(fname="c:/Windows/Fonts/malgun.ttf").get_name()
         rc('font', family=font_name)
     else:    
-    # Mac 인 경우
         rc('font', family='AppleGothic')
-except :
+except:
     pass
 matplotlib.rcParams['axes.unicode_minus'] = False  # 마이너스 기호 깨짐 방지
 
@@ -27,15 +25,14 @@ class PensionData():
     # 생성자: 데이터파일 읽어온후 전처리 수행
     def __init__(self, filepath):
         warnings.simplefilter(action='ignore', category=pd.errors.DtypeWarning)
-        self.df = pd.read_csv(os.path.join(filepath), encoding='cp949')
+        self.df = pd.read_csv(filepath, encoding='cp949')
         self.pattern1 = r'(\([^)]+\))'
         self.pattern2 = r'(\[[^)]+\])'
         self.pattern3 = r'[^A-Za-z0-9가-힣]'
         self.preprocess()
     
-    # 전처리   
+    # 전처리    
     def preprocess(self):
-
         # '사업장업종코드' 컬럼값이 빈 문자열인것들은 제거한다.
         mask = self.df['사업장업종코드'].replace({r'^\s+$': pd.NA}, regex=True).isna()
         self.df = self.df[~mask]
@@ -52,11 +49,11 @@ class PensionData():
         ]
         # 불필요한 컬럼 제거
         df = self.df.drop(['자료생성년월', '우편번호', '사업장지번상세주소', '고객법정동주소코드', '고객행정동주소코드', '사업장형태구분코드 1 법인 2 개인', '적용일자', '재등록일자'], axis=1)
-        # 사업장명 cleasing
+        # 사업장명 cleansing
         df['사업장명'] = df['사업장명'].apply(self.preprocessing)
         # '탈퇴일자_연도', '탈퇴일자_월' 컬럼 추가
-        df['탈퇴일자_연도'] =  pd.to_datetime(df['탈퇴일자']).dt.year
-        df['탈퇴일자_월'] =  pd.to_datetime(df['탈퇴일자']).dt.month
+        df['탈퇴일자_연도'] = pd.to_datetime(df['탈퇴일자']).dt.year
+        df['탈퇴일자_월'] = pd.to_datetime(df['탈퇴일자']).dt.month
         # '주소' 컬럼에서 '시도' 부분만 새 컬럼으로 추가
         df['시도'] = df['주소'].str.split(' ').str[0]
 
@@ -65,14 +62,13 @@ class PensionData():
 
         # 분석하고자 하는 컬럼들 추가
         df['인당금액'] = df['금액'] / df['가입자수']
-        df['월급여추정'] =  df['인당금액'] / 9 * 100
+        df['월급여추정'] = df['인당금액'] / 9 * 100
         df['연간급여추정'] = df['월급여추정'] * 12
         
         self.df = df  # 원본 변경하기    
 
     # 사업장명 정제를 위한 함수    
     def preprocessing(self, x):
-        # 특수 문자들 제거.   "(주)", "[주]" ...
         x = re.sub(self.pattern1, '', x)
         x = re.sub(self.pattern2, '', x)
         x = re.sub(self.pattern3, ' ', x)
@@ -81,12 +77,9 @@ class PensionData():
 
     # 주어진 'company_name' 으로 기업 검색
     def find_company(self, company_name):
-        # 가입자수가 많은 순으로 정렬하여 리턴
-        return self.df.loc[self.df['사업장명'].str.contains(company_name), ['사업장명', '월급여추정', '연간급여추정', '업종코드', '가입자수']]\
-                  .sort_values('가입자수', ascending=False)
+        return self.df.loc[self.df['사업장명'].str.contains(company_name), ['사업장명', '월급여추정', '연간급여추정', '업종코드', '가입자수']].sort_values('가입자수', ascending=False)
 
-    # 주어진 'company_name' 으로 
-    # 동종업계 정보 (월급여 추정액, 연간급여추정액 ) 비교 
+    # 동종업계 정보 비교 
     def compare_company(self, company_name):
         company = self.find_company(company_name)
         code = company['업종코드'].iloc[0]
@@ -106,10 +99,10 @@ class PensionData():
     def get_data(self):
         return self.df 
 
-# 국민연금공단_국민연금 가입 사업장 내역_20251124.csv
+
 file_path = r'https://www.dropbox.com/scl/fi/q05nabk8r0822dy8q1kew/_-_20251124.csv?rlkey=x3z852i71fwm60kc69rijiwno&st=cxcnw7rz&dl=1'
 
-@st.cache_resource # 동일 함수가 반복 호출되면 매번 실행하지 않고 마지막으로 실행된 결과를 리턴하게 함
+@st.cache_resource
 def read_pensiondata():
     data = PensionData(file_path)
     return data
@@ -121,12 +114,12 @@ st.title('국민연금 데이터 분석')
 company_name = st.text_input('회사명을 입력해주세요', placeholder='검색할 회사명 입력')
 
 if data and company_name:
-    output = data.find_company(company_name = company_name)
+    output = data.find_company(company_name=company_name)
 
     if len(output) > 0:
         st.subheader(output.iloc[0]['사업장명'])
 
-        info = data.company_info(company_name = company_name)
+        info = data.company_info(company_name=company_name)
         st.markdown(
             f"""
             - `{info['주소']}`
@@ -137,61 +130,63 @@ if data and company_name:
             """
         ) 
 
-col1, col2, col3 = st.columns(3)
+        col1, col2, col3 = st.columns(3)
 
-col1.text('월급여 추정')
-col1.markdown(f"`{int(output.iloc[0]['월급여추정']):,}` 원")
+        col1.text('월급여 추정')
+        col1.markdown(f"`{int(output.iloc[0]['월급여추정']):,}` 원")
 
-col2.text('연봉 추정')
-col2.markdown(f"`{int(output.iloc[0]['연간급여추정']):,}` 원")
+        col2.text('연봉 추정')
+        col2.markdown(f"`{int(output.iloc[0]['연간급여추정']):,}` 원")
 
-col3.text('가입자수 추정')
-col3.markdown(f"`{int(output.iloc[0]['가입자수']):,}` 명")
+        col3.text('가입자수 추정')
+        col3.markdown(f"`{int(output.iloc[0]['가입자수']):,}` 명")
 
-comp_output = data.compare_company(company_name = company_name)
-st.dataframe(comp_output.round(0), use_container_width=True)
- 
-st.markdown(f'### 업종 평균 VS {company_name} 비교')
+        comp_output = data.compare_company(company_name=company_name)
+        st.dataframe(comp_output.round(0), use_container_width=True)
+         
+        st.markdown(f'### 업종 평균 VS {company_name} 비교')
 
-# 검색은 회사의 '월급여추정'액과 업종평균을 비교
-percent_value = info['월급여추정'] / comp_output.iloc[0, 0] * 100 - 100
-diff_month = abs(comp_output.iloc[0, 0] - info['월급여추정'])  # 월급여추정 액의 차이
-diff_year = abs(comp_output.iloc[1, 0] - info['연간급여추정'])  # 연간급여추정 액의 차이
-upordown = '높은' if percent_value > 0 else '낮은'  # %값이 높은지 낮은지에 따른 문구 선택 
+        percent_value = info['월급여추정'] / comp_output.iloc[0, 0] * 100 - 100
+        diff_month = abs(comp_output.iloc[0, 0] - info['월급여추정'])
+        diff_year = abs(comp_output.iloc[1, 0] - info['연간급여추정'])
+        upordown = '높은' if percent_value > 0 else '낮은'
 
-# 위 결과로 아래에 markdown 으로 출력
-st.markdown(f"""
-- 업종 **평균 월급여**는 `{int(comp_output.iloc[0, 0]):,}` 원, **평균 연봉**은 `{int(comp_output.iloc[1, 0]):,}` 원 입니다.
-- `{company_name}`는 평균 보다 `{int(diff_month):,}` 원, :red[약 {percent_value:.2f} %] `{upordown}` `{int(info['월급여추정']):,}` 원을 **월 평균 급여**를 받는 것으로 추정합니다.
-- `{company_name}`는 평균 보다 `{int(diff_year):,}` 원 `{upordown}` `{int(info['연간급여추정']):,}` 원을 **연봉**을 받는 것으로 추정합니다.
-""")
+        st.markdown(f"""
+        - 업종 **평균 월급여**는 `{int(comp_output.iloc[0, 0]):,}` 원, **평균 연봉**은 `{int(comp_output.iloc[1, 0]):,}` 원 입니다.
+        - `{company_name}`는 평균 보다 `{int(diff_month):,}` 원, :red[약 {percent_value:.2f} %] `{upordown}` `{int(info['월급여추정']):,}` 원을 **월 평균 급여**를 받는 것으로 추정합니다.
+        - `{company_name}`는 평균 보다 `{int(diff_year):,}` 원 `{upordown}` `{int(info['연간급여추정']):,}` 원을 **연봉**을 받는 것으로 추정합니다.
+        """)
 
-fig, ax = plt.pyplot(1, 2)
-p1 = ax[0].bar(x=["Average", "Your Company"], height=(comp_output.iloc[0, 0], info['월급여추정']), width=0.7)
-ax[0].bar_label(p1, fmt='%d')
-p1[0].set_color('black')
-p1[1].set_color('red')
-ax[0].set_title('Monthly Salary')
+        # plt.pyplot(1, 2) -> plt.subplots(1, 2) 로 수정
+        fig, ax = plt.subplots(1, 2)
+        p1 = ax[0].bar(x=["Average", "Your Company"], height=(comp_output.iloc[0, 0], info['월급여추정']), width=0.7)
+        ax[0].bar_label(p1, fmt='%d')
+        p1[0].set_color('black')
+        p1[1].set_color('red')
+        ax[0].set_title('Monthly Salary')
 
-p2 = ax[1].bar(x=["Average", "Your Company"], height=(comp_output.iloc[1, 0], info['연간급여추정']), width=0.7)
-p2[0].set_color('black')
-p2[1].set_color('red')
-ax[1].bar_label(p2, fmt='%d')
-ax[1].set_title('Yearly Salary')
+        p2 = ax[1].bar(x=["Average", "Your Company"], height=(comp_output.iloc[1, 0], info['연간급여추정']), width=0.7)
+        p2[0].set_color('black')
+        p2[1].set_color('red')
+        ax[1].bar_label(p2, fmt='%d')
+        ax[1].set_title('Yearly Salary')
 
-ax[0].tick_params(axis='both', which='major', labelsize=8, rotation=0)
-ax[0].tick_params(axis='both', which='minor', labelsize=6)
-ax[1].tick_params(axis='both', which='major', labelsize=8)
-ax[1].tick_params(axis='both', which='minor', labelsize=6)
+        ax[0].tick_params(axis='both', which='major', labelsize=8, rotation=0)
+        ax[0].tick_params(axis='both', which='minor', labelsize=6)
+        ax[1].tick_params(axis='both', which='major', labelsize=8)
+        ax[1].tick_params(axis='both', which='minor', labelsize=6)
 
-st.pyplot(fig)
+        st.pyplot(fig)
 
-st.markdown('### 동종업계')
-df = data.get_data()
-st.dataframe(df.loc[df['업종코드'] == info['업종코드'], ['사업장명', '월급여추정', '연간급여추정', '가입자수']]\
-            .sort_values('연간급여추정', ascending=False).head(10).round(0), 
+        st.markdown('### 동종업계')
+        df = data.get_data()
+        st.dataframe(
+            df.loc[df['업종코드'] == info['업종코드'], ['사업장명', '월급여추정', '연간급여추정', '가입자수']]
+            .sort_values('연간급여추정', ascending=False)
+            .head(10)
+            .round(0), 
             use_container_width=True
         )
 
     else:
-          st.subheader('검색결과가 없습니다')
+        st.subheader('검색결과가 없습니다')
