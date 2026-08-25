e4f1302094caf4b9906ca39184894be5f8347e27
hjaeqwer-art <hjaeqwer@gmail.com>
2026-08-25 20:18:52 +0900

    Update streamlit_main.py

--- streamlit_main.py
+++ streamlit_main.py
@@ -186,9 +186,9 @@ ax[1].tick_params(axis='both', which='minor', labelsize=6)
 
 st.pyplot(fig)
 
-        st.markdown('### 동종업계')
-        df = data.get_data()
-        st.dataframe(df.loc[df['업종코드'] == info['업종코드'], ['사업장명', '월급여추정', '연간급여추정', '가입자수']]\
+st.markdown('### 동종업계')
+df = data.get_data()
+st.dataframe(df.loc[df['업종코드'] == info['업종코드'], ['사업장명', '월급여추정', '연간급여추정', '가입자수']]\
             .sort_values('연간급여추정', ascending=False).head(10).round(0), 
             use_container_width=True
         )
